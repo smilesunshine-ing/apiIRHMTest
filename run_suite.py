c4b650b2298ed8507e00ec7b9c312c37bf079583
smilesunshine-ing <59458039+smilesunshine-ing@users.noreply.github.com>
2020-01-08 12:04:31 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -14,7 +14,7 @@ suite.addTest(unittest.makeSuite(Login))
 suite.addTest(unittest.makeSuite(TestIHRMEmp))
 # suite.addTest(unittest.makeSuite(TestIHRMLogin))
 # 3.使用HTMLRunner 执行测试套件，生成测试报告
-report_path = app.BASE_DIR + '/report/ihrm{}.html'.format(time.strftime('%Y%m%d-%H%M%S'))
+report_path = app.BASE_DIR + '/report/ihrm.html'
 with open(report_path, 'wb') as f:
     # 初始化HTMLRunner
     runner = HTMLTestRunner(f, verbosity=1, title='IHRM人力资源接口测试', description='v1.0.0')
